e959a4ca10b203a47c466fe7914ba6d69df761bf
Kyle Blagg <noahjemkai@gmail.com>
2021-12-18 00:38:07 -0500

    WIP readying for next step in implementation

--- src/TorrentRequest.py
+++ src/TorrentRequest.py
@@ -1,35 +1,40 @@
 from __future__ import annotations
 from datetime import datetime
+from logging import makeLogRecord
 from transmission_rpc.torrent import Torrent
 
 from .Enums import TorrentRequestStatus, TorrentType
 
-class TorrentRequest:
+class TorrentRequest(object):
     def __init__(
         self,
         hash: str,
         status: TorrentRequestStatus,
         type: TorrentType = None,
-        id: int = None,
         name: str = None,
         description: str = None,
         target_name: str = None,
         computed_name: str = None,
         cur_dir: str = None,
         dest_dir: str = None,
+        magnet: str = None,
+        added_by: str = None, # User who added the request
+        requires_review: bool = True,
         created_at: datetime = None,
         updated_at: datetime = None
     ) -> None:
         self.hash = hash
         self.status = status
         self.type = type
-        self.id = id
         self.name = name
         self.description = description
         self.target_name = target_name
         self.computed_name = computed_name
         self.cur_dir = cur_dir
         self.dest_dir = dest_dir
+        self.magnet = magnet
+        self.added_by = added_by
+        self.requires_review = requires_review
         self.created_at = created_at if created_at is not None else datetime.utcnow()
         self.updated_at = updated_at if updated_at is not None else datetime.utcnow()
 
@@ -52,19 +57,59 @@ class TorrentRequest:
         else:
             return TorrentRequestStatus.STOPPED if torrent.progress != 1 else TorrentRequestStatus.PENDING_REVIEW
 
-    def __str__(self) -> str:
+    @classmethod
+    def fromDict(cls, source: dict) -> TorrentRequestStatus:
+        return cls(
+            hash=source.get('hash'),
+            status=TorrentRequestStatus[dict.get('status', 'QUEUED')],
+            type=TorrentType[dict.get('type')] if dict.get('type') is not None else None,
+            name=dict.get('name'),
+            description=dict.get('description'),
+            target_name=dict.get('target_name'),
+            computed_name=dict.get('computed_name'),
+            cur_dir=dict.get('cur_dir'),
+            dest_dir=dict.get('dest_dir'),
+            magnet=dict.get('magnet'),
+            added_by=dict.get('added_by'),
+            requires_review=dict.get('requires_review', True),
+            created_at=dict.get('created_at'),
+            updated_at=dict.get('updated_at'),
+        )
+
+    def toDict(self):
+        return {
+            'hash' : self.hash,
+            'status' : self.status,
+            'type' : self.type,
+            'name' : self.name,
+            'description' : self.description,
+            'target_name' : self.target_name,
+            'computed_name' : self.computed_name,
+            'cur_dir' : self.cur_dir,
+            'dest_dir' : self.dest_dir,
+            'magnet' : self.magnet,
+            'added_by' : self.added_by,
+            'requires_review' : self.requires_review,
+            'created_at' : self.created_at,
+            'updated_at' : self.updated_at,
+        }
+
+    def __repr__(self) -> str:
         return (
-        f'{self.__class__} '
-        f'hash: {self.hash} '
-        f'status: {self.status} '
-        f'type: {self.type} '
-        f'id: {self.id} '
-        f'name: {self.name} '
-        f'description: {self.description} '
-        f'target_name: {self.target_name} '
-        f'computed_name: {self.computed_name} '
-        f'cur_dir: {self.cur_dir} '
-        f'dest_dir: {self.dest_dir} '
-        f'created_at: {self.created_at} '
-        f'updated_at: {self.updated_at} '
+            f'TorrentRequest(\
+                hash={self.hash}\
+                name={self.name}\
+                status={self.status}\
+                type={self.type}\
+                description={self.description}\
+                target_name={self.target_name}\
+                computed_name={self.computed_name}\
+                cur_dir={self.cur_dir}\
+                dest_dir={self.dest_dir}\
+                magnet={self.magnet}\
+                added_by={self.added_by}\
+                requires_review={self.requires_review}\
+                created_at={self.created_at}\
+                updated_at={self.updated_at}\
+            )'
         )
--- sync.py
+++ sync.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
 
 import configparser
+import argparse
 from transmission_rpc import Client
 from src.TorrentRequest import TorrentRequest
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+parser = argparse.ArgumentParser(description='Sync torrent states with torrent requests DB')
+
 
 config = configparser.ConfigParser()
 
@@ -17,4 +24,16 @@ c = Client(username=transmission_config['username'], password=transmission_confi
 
 torrents = c.get_torrents()
 
-print([str(TorrentRequest.fromTorrent(t)) for t in torrents])
+# TODO: Now that existing torrents are converted into TorrentRequest objects, lets sync them with firestore if necessary.
+torrent_reqs = [TorrentRequest.fromTorrent(t) for t in torrents]
+#print(torrent_reqs)
+
+if 'firestore' not in config or not config['firestore']['service_account_key_path']:
+    raise ConnectionError('Missing required firestore config key(s).  Please check config.ini file.')
+
+cred = credentials.Certificate(config['firestore']['service_account_key_path'])
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+[db.collection(u'torrents').document(t_req.hash).add(t_req.toDict()) for t_req in torrent_reqs]
